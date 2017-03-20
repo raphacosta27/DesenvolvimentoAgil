39be050d37e07b8ca270006bc17b6560cbf8b742
raphacosta27 <rapha2706@hotmail.com>
2017-03-20 19:30:34 -0300

    Mux

--- desagil-lucianogic-0.2/src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/MuxGate.java
+++ desagil-lucianogic-0.2/src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/MuxGate.java
@@ -7,11 +7,15 @@ public class MuxGate extends Gate {
 	private NandGate nandGate4;
 
 	public MuxGate() {
-		super(2);
+		super(3);
 		nandGate = new NandGate();
 		nandGate2 = new NandGate();
 		nandGate3 = new NandGate();
 		nandGate4 = new NandGate();
+		nandGate2.connect(nandGate, 1);
+		nandGate4.connect(nandGate2, 0);
+		nandGate4.connect(nandGate3, 1);
+		
 	}
 
 	@Override
@@ -22,12 +26,15 @@ public class MuxGate extends Gate {
 	@Override
 	protected void doConnect(Emitter emitter, int index) {
 		if (index == 0){
-			nandGate.connect(emitter, 0);
 			nandGate2.connect(emitter, 0);
 		}
-		else{
-			nandGate.connect(emitter, 1);
+		else if(index == 1){
 			nandGate3.connect(emitter, 1);
+		}
+		else {
+			nandGate.connect(emitter, 0);
+			nandGate.connect(emitter, 1);
+			nandGate3.connect(emitter, 0);
 		}
 	}
 }
